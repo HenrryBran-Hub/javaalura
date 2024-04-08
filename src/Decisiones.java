141e9a5c6c1f3b6d4758af6d39ad1b2942fcc454
Henrry Bran <2168036320108@ingenieria.usac.edu.gt>
2024-04-07 22:49:22 -0600

    if-else

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -5,11 +5,18 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnElPlan = true;
         double notaDeLaPelicula = 8.2;
+        String tipoPlan = "plus";
 
         if (fechaDeLanzamiento >= 2022){
             System.out.println("Peliculas más populares");
         }else{
             System.out.println("Peliculas retro que aun vale la pnea ver");
+        }
+
+        if(incluidoEnElPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su pelicula");
+        }else{
+            System.out.println("Pelicula no disponible para su plan actual");
         }
     }
 }
