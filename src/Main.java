badd587a60e86d4bff05342ca6bf6e459d05bf6d
Henrry Bran <2168036320108@ingenieria.usac.edu.gt>
2024-04-07 22:34:14 -0600

    agregamos nueva clase y if-else

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Bienvenido(a) a Screen Match");
+        System.out.println("Película: Matriz");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0 )/ 3;
+
+        System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja
+                la mejor película del milenio
+                Fué lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media / 2);
+        System.out.println(clasificacion);
+
     }
 }
